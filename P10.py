bce2fdecf4afcff42fc0089242a5b14c2118343b
Krish <krishbhardwaj27203@gmail.com>
2022-01-04 22:24:15 +0530

    Project euler late sub update , jan 4

--- P10.py
+++ P10.py
@@ -1,3 +1,24 @@
-import eulerlib
-if __name__ == "__main__":
-    print(sum(eulerlib.list_primes(1999999)))
+import math
+def check_prime(num):
+    if num > 2 and num % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(math.sqrt(num)) + 1, 2):
+            if num % i == 0:
+                return False
+    return True
+
+
+def find_sum(limit):
+    sum = 0
+    for i in range(2, limit):
+        if check_prime(i):
+            sum += i
+
+    return sum
+
+
+if __name__ == '__main__':
+    print(find_sum(2000000))
+    # test
+    print(find_sum(10))
